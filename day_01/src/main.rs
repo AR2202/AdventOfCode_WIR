d36dd0a5d838918e516f0605f5d724d351cf44fa
AnnikaRings <annika.rings@optellum.com>
2024-12-02 22:47:29 +0000

    ran formatter

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -10,21 +10,27 @@ fn read_input(filename: &str) -> Vec<Vec<i32>> {
     let contents = fs::read_to_string(filename).unwrap();
     let numbers: Vec<Vec<i32>> = contents
         .lines()
-        .map(|line| line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect())
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect()
+        })
         .collect();
     numbers
 }
 
-fn sep_lists(numlist:Vec<Vec<i32>> ) -> Vec<( i32,i32)> {
+fn sep_lists(numlist: Vec<Vec<i32>>) -> Vec<(i32, i32)> {
     let mut first_list = numlist.iter().map(|l| l[0]).collect::<Vec<i32>>();
     first_list.sort();
     let mut second_list = numlist.iter().map(|l| l[1]).collect::<Vec<i32>>();
     second_list.sort();
-    let zipped = first_list.into_iter().zip(second_list.into_iter()).collect();
+    let zipped = first_list
+        .into_iter()
+        .zip(second_list.into_iter())
+        .collect();
     zipped
 }
 
-fn sum_differences(numtuples:Vec<( i32, i32)>) -> i32{
-   
-    numtuples.iter().map(|l| (l.0-l.1).abs()).sum()
+fn sum_differences(numtuples: Vec<(i32, i32)>) -> i32 {
+    numtuples.iter().map(|l| (l.0 - l.1).abs()).sum()
 }
