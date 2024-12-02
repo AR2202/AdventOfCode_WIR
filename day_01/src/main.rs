9298d90dce96114bf752095d6684ba639d57da78
AnnikaRings <annika.rings@optellum.com>
2024-12-02 22:44:58 +0000

    added Day 1 Part 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,30 @@
+use std::{convert::TryInto, fs};
 fn main() {
     println!("Welcome to Day 1!");
+    let nums = read_input("input/Day01.txt");
+    let sorted_lists = sep_lists(nums);
+    let diffs = sum_differences(sorted_lists);
+    println!("{:?}", diffs)
+}
+fn read_input(filename: &str) -> Vec<Vec<i32>> {
+    let contents = fs::read_to_string(filename).unwrap();
+    let numbers: Vec<Vec<i32>> = contents
+        .lines()
+        .map(|line| line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect())
+        .collect();
+    numbers
+}
+
+fn sep_lists(numlist:Vec<Vec<i32>> ) -> Vec<( i32,i32)> {
+    let mut first_list = numlist.iter().map(|l| l[0]).collect::<Vec<i32>>();
+    first_list.sort();
+    let mut second_list = numlist.iter().map(|l| l[1]).collect::<Vec<i32>>();
+    second_list.sort();
+    let zipped = first_list.into_iter().zip(second_list.into_iter()).collect();
+    zipped
+}
+
+fn sum_differences(numtuples:Vec<( i32, i32)>) -> i32{
+   
+    numtuples.iter().map(|l| (l.0-l.1).abs()).sum()
 }
