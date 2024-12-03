1aa52d820bfdc5a56e7498de3841c9d1a2dc554b
AnnikaRings <annika.rings@optellum.com>
2024-12-03 22:23:11 +0000

    some refactoring

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,30 +1,23 @@
 use std::{convert::TryInto, fs};
-///solving both parts of day 1
+///solving both parts of day 1 and printing
 fn main() {
     println!("Welcome to Day 1!");
-    let nums = read_input("input/Day01.txt");
-    let first_list = extract_first_list(&nums);
-    let second_list = extract_second_list(&nums);
-    let zipped_lists = zip_lists(&first_list, &second_list);
-    let diffs = sum_differences(&zipped_lists);
-    let product_with_counts = mult_by_count(&first_list, &second_list);
-    println!("Day 1 Part 1: {:?}", diffs);
-    println!("Day 1 Part 2: {:?}", product_with_counts);
+    let (part1, part2) = sol_day1("input/Day01.txt");
+    println!("Day 1 Part 1: {:?}", part1);
+    println!("Day 1 Part 2: {:?}", part2);
 }
-/// solving part 1 of day 1 - used for testing
-fn sol_day1_part1(filename: &str) -> i32 {
+
+/// solving  day 1 from file input
+fn sol_day1(filename: &str) -> (i32, i32) {
     let nums = read_input(filename);
-    let first_list = extract_first_list(&nums);
-    let second_list = extract_second_list(&nums);
+    let (first_list, second_list) = extract_sep_lists(&nums);
     let zipped_lists = zip_lists(&first_list, &second_list);
-    sum_differences(&zipped_lists)
+    (
+        sum_differences(&zipped_lists),
+        mult_by_count(&first_list, &second_list),
+    )
 }
-fn sol_day1_part2(filename: &str) -> i32 {
-    let nums = read_input(filename);
-    let first_list = extract_first_list(&nums);
-    let second_list = extract_second_list(&nums);
-    mult_by_count(&first_list, &second_list)
-}
+/// reading input from file and returning list of lists
 fn read_input(filename: &str) -> Vec<Vec<i32>> {
     let contents = fs::read_to_string(filename).unwrap();
     let numbers: Vec<Vec<i32>> = contents
@@ -37,17 +30,15 @@ fn read_input(filename: &str) -> Vec<Vec<i32>> {
         .collect();
     numbers
 }
-
-fn extract_first_list(numlist: &Vec<Vec<i32>>) -> Vec<i32> {
+/// separating the two lists and sorting them
+fn extract_sep_lists(numlist: &Vec<Vec<i32>>) -> (Vec<i32>, Vec<i32>) {
     let mut first_list = numlist.iter().map(|l| l[0]).collect::<Vec<i32>>();
     first_list.sort();
-    first_list
-}
-fn extract_second_list(numlist: &Vec<Vec<i32>>) -> Vec<i32> {
     let mut second_list = numlist.iter().map(|l| l[1]).collect::<Vec<i32>>();
     second_list.sort();
-    second_list
+    (first_list, second_list)
 }
+/// zipping lists to list of tuples
 fn zip_lists<'a>(first_list: &'a Vec<i32>, second_list: &'a Vec<i32>) -> Vec<(&'a i32, &'a i32)> {
     let zipped = first_list
         .into_iter()
@@ -55,10 +46,11 @@ fn zip_lists<'a>(first_list: &'a Vec<i32>, second_list: &'a Vec<i32>) -> Vec<(&'
         .collect();
     zipped
 }
+/// sum of differences of the tuple values
 fn sum_differences(numtuples: &Vec<(&i32, &i32)>) -> i32 {
     numtuples.iter().map(|l| (l.0 - l.1).abs()).sum()
 }
-
+/// count occurrances of element in vector
 fn count_occurrances(elem: i32, list: &Vec<i32>) -> i32 {
     list.iter()
         .filter(|&n| *n == elem)
@@ -66,7 +58,7 @@ fn count_occurrances(elem: i32, list: &Vec<i32>) -> i32 {
         .try_into()
         .unwrap()
 }
-
+/// multiply each element by its number of occurrances
 fn mult_by_count(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
     list1
         .iter()
@@ -82,10 +74,10 @@ mod tests {
 
     #[test]
     fn day1a_test() {
-        assert_eq!(sol_day1_part1("input/Day01example.txt"), 11);
+        assert_eq!(sol_day1("input/Day01example.txt").0, 11);
     }
     #[test]
     fn day1b_test() {
-        assert_eq!(sol_day1_part2("input/Day01example.txt"), 31);
+        assert_eq!(sol_day1("input/Day01example.txt").1, 31);
     }
 }
