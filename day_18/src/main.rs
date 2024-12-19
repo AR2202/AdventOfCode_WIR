76826eb27ecafc97ba779f543e039fb9a3921fa0
AnnikaRings <annika.rings@optellum.com>
2024-12-19 18:49:43 +0000

    added day 18

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,50 +1,77 @@
-use petgraph::algo::astar;
+use petgraph::adj::NodeIndex;
+use petgraph::algo::{all_simple_paths, astar};
+use petgraph::graph::{Graph, Neighbors};
 use std::collections::HashSet;
 use std::fs;
-
-use petgraph::graph::{Graph, Neighbors};
 fn main() {
     println!("Welcome to Day 18!");
     let contents = fs::read_to_string("input/day18ex.txt").expect("file not found");
-    let fallen_bytes = make_bytes_dict(&contents,12);
-    let example_graph = make_graph(&fallen_bytes,7);
-    
+    let (fallen_bytes, rest_ex) = make_bytes_dict(&contents, 12);
+    let example_graph = make_graph(&fallen_bytes, 7);
+
     let startpoint = 0;
     let endpoint = 48;
-    println!("example: {:?} ",find_shortest_path(example_graph, startpoint, endpoint));
+    println!(
+        "example: {:?} ",
+        find_shortest_path(example_graph.clone(), startpoint, endpoint)
+    );
+    println!(
+        "part2 example: {} ",
+        try_all_bytes(&contents, startpoint, endpoint, 12,7));
     let contents_inp = fs::read_to_string("input/day18.txt").expect("file not found");
-    let fallen_bytes_inp = make_bytes_dict(&contents_inp,1024);
-    let graph = make_graph(&fallen_bytes_inp,71);
-   
+    let (fallen_bytes_inp, rest) = make_bytes_dict(&contents_inp, 1024);
+    let graph = make_graph(&fallen_bytes_inp, 71);
+
     let startpoint_p1 = 0;
     let endpoint_p1 = 5040;
-    println!("part1: {:?} ",find_shortest_path(graph, startpoint_p1, endpoint_p1));
+    println!(
+        "part1: {:?} ",
+        find_shortest_path(graph.clone(), startpoint_p1, endpoint_p1)
+    );
+    println!(
+        "part2: {} ",
+        try_all_bytes(&contents_inp, startpoint_p1, endpoint_p1, 1024,71));
+    // println!(
+    //     "part2: {:?} ",
+    //     find_blocking_all_paths(&graph, startpoint_p1, endpoint_p1, rest)
+    // );
 }
-fn make_bytes_dict(contents: &str, nbytes:usize) -> HashSet<(u32, u32)> {
- let linevec: Vec<&str> = contents.lines().collect();
- linevec[0..nbytes].iter().map(|line|line.split(",").map(|n| n.parse::<u32>().unwrap()).collect()).map(|v:Vec<u32>|(v[0],v[1])).collect()
+fn make_bytes_dict(contents: &str, nbytes: usize) -> (HashSet<(u32, u32)>, Vec<(u32, u32)>) {
+    let linevec: Vec<&str> = contents.lines().collect();
+    (
+        linevec[0..nbytes]
+            .iter()
+            .map(|line| line.split(",").map(|n| n.parse::<u32>().unwrap()).collect())
+            .map(|v: Vec<u32>| (v[0], v[1]))
+            .collect(),
+        linevec[nbytes..]
+            .iter()
+            .map(|line| line.split(",").map(|n| n.parse::<u32>().unwrap()).collect())
+            .map(|v: Vec<u32>| (v[0], v[1]))
+            .collect(),
+    )
 }
 
-fn make_graph(bytes_dict:&HashSet<(u32,u32)>, limits: u32) -> Graph<u32,u32>{
+fn make_graph(bytes_dict: &HashSet<(u32, u32)>, limits: u32) -> Graph<u32, u32> {
     let mut edgevec = Vec::new();
-    for x in 0..limits{
-        for y in 0..limits{
-            if !bytes_dict.contains(&(x,y)){
-                if x>0 && !bytes_dict.contains(&(x-1,y)){
-                    edgevec.push((y*limits+x, y*limits+x-1,1));
-                    edgevec.push((y*limits+x-1, y*limits+x,1));
+    for x in 0..limits {
+        for y in 0..limits {
+            if !bytes_dict.contains(&(x, y)) {
+                if x > 0 && !bytes_dict.contains(&(x - 1, y)) {
+                    edgevec.push((y * limits + x, y * limits + x - 1, 1));
+                    edgevec.push((y * limits + x - 1, y * limits + x, 1));
                 }
-                if  x < limits - 1 && !bytes_dict.contains(&(x+1,y)){
-                    edgevec.push((y*limits+x, y*limits+x+1,1));
-                    edgevec.push((y*limits+x+1, y*limits+x,1));
+                if x < limits - 1 && !bytes_dict.contains(&(x + 1, y)) {
+                    edgevec.push((y * limits + x, y * limits + x + 1, 1));
+                    edgevec.push((y * limits + x + 1, y * limits + x, 1));
                 }
-                if y>0 && !bytes_dict.contains(&(x,y-1)){
-                    edgevec.push(((y-1)*limits+x, y*limits+x,1));
-                    edgevec.push((y*limits+x,(y-1)*limits+x, 1));
+                if y > 0 && !bytes_dict.contains(&(x, y - 1)) {
+                    edgevec.push(((y - 1) * limits + x, y * limits + x, 1));
+                    edgevec.push((y * limits + x, (y - 1) * limits + x, 1));
                 }
-                if y < limits - 1  && !bytes_dict.contains(&(x,y+1)){
-                    edgevec.push(((y+1)*limits+x, y*limits+x,1));
-                    edgevec.push((y*limits+x,(y+1)*limits+x, 1));
+                if y < limits - 1 && !bytes_dict.contains(&(x, y + 1)) {
+                    edgevec.push(((y + 1) * limits + x, y * limits + x, 1));
+                    edgevec.push((y * limits + x, (y + 1) * limits + x, 1));
                 }
             }
         }
@@ -64,4 +91,55 @@ fn find_shortest_path(graph: Graph<u32, u32>, startpoint: u32, endpoint: u32) ->
         Some((w, p)) => w,
         _ => 0,
     }
+}
+/// this finds the first one that blocks the path
+/// for every simple path
+/// too inefficient 
+fn find_blocking_all_paths(
+    g: &Graph<u32, u32>,
+    startpoint: u32,
+    endpoint: u32,
+    bytes: Vec<(u32, u32)>,
+) -> (usize, (u32, u32)) {
+    let mut first_blocking =
+        all_simple_paths::<Vec<_>, _>(g, startpoint.into(), endpoint.into(), 0, None).map(|path| {
+            let path_as_u32: Vec<u32> = path.into_iter().map(|node| node.index() as u32).collect();
+            find_first_byte(path_as_u32, &bytes)
+        });
+       
+    
+    first_blocking.fold(
+        (0, (0, 0)),
+        |(acci, accb), (i, b)| {
+            if i > acci {
+                (i, b)
+            } else {
+                (acci, accb)
+            }
+        },
+    )
+}
+/// this finds the first byte that blocks a path
+fn find_first_byte(path: Vec<u32>, bytes: &Vec<(u32, u32)>) -> (usize, (u32, u32)) {
+    for (i, b) in bytes.iter().enumerate() {
+        if path.contains(&(b.0 + 71 * b.1)) {
+            return (i, *b);
+        }
+    }
+    return (1000000, (0, 0));
+}
+
+fn try_all_bytes(contents_inp:&str, startpoint_p1:u32, endpoint_p1:u32, fallen:usize,limits:u32)->&str{
+    let ls = contents_inp.lines().collect::<Vec<&str>>();
+    for num_bytes in fallen..ls.len(){
+        let (fallen_bytes_inp, rest) = make_bytes_dict(contents_inp, num_bytes);
+        let graph = make_graph(&fallen_bytes_inp, limits);
+        if find_shortest_path(graph, startpoint_p1, endpoint_p1) == 0{
+            return ls[num_bytes-1];
+        }
+    
+        
+    }
+   
+    "not found"
 }
